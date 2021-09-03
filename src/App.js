e3849cef2224c777d62690dac0e3201710a8b209
Louati-Alaeddine <76881640+Louati-Alaeddine@users.noreply.github.com>
2021-09-03 21:05:45 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+import UserList from "./UserList";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>API items</h1>
+      <UserList />
+    </div>
+  );
+}
+
+export default App;
